4588626d879802b37bbd8452541cfbef744bd28b
Youcheng Zhang <zhangyc.life@gmail.com>
2022-05-10 00:55:51 +0800

    [add] add support for -k option that only print TOTP key

--- login.sh
+++ login.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 set -e
-SERVER="$1"
-BASEDIR=$(dirname "$0")
 
+# Start reading argument from second place
+OPTIND=2
+while getopts ":k" opt; do
+	case $opt in
+    k)
+			k=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2 && exit 1
+      ;;
+  esac
+done
+
+BASEDIR=$(dirname "$0")
+SERVER="$1" # Server name is always the first argument
 source "$BASEDIR/$SERVER.param"
 
-TOTP=$(python3 $BASEDIR/mintotp.py $TOTP_KEY)
+# Get TOTP key
+TOTP=$(python3 "$BASEDIR"/mintotp.py "$TOTP_KEY")
 
-expect "$BASEDIR/login.exp" "$SERVER" "$PASSWD" "$TOTP"
+if [ "$k" = true ] ; then
+	echo "$TOTP"
+else
+	expect "$BASEDIR/login.exp" "$SERVER" "$PASSWD" "$TOTP"
+fi
